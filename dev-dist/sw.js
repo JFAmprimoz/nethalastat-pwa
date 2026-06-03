c02054e954af61ee071367326b039293e49287f0
JFA <jamprimoz@gmail.com>
2026-06-03 13:56:21 -0400

    get icons up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vbtjm6gdqis"
+    "revision": "0.872rbq1e08s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
