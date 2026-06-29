8f9333755f3e8aeb3f4935282b14922fa2dbcf3b
JFA <jamprimoz@gmail.com>
2026-06-29 12:23:59 -0400

    update about and terms for new cc email

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hbb86j52r6c"
+    "revision": "0.8ovgfc5igjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
